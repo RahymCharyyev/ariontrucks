03fc2c309acbf9776cf1e823caf06543f4912314
RahymCharyyev <r569837@gmail.com>
2024-04-25 21:13:22 +0500

    🐞 fix: Removed photo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,18 +1,18 @@
 import { getI18n } from '@/locales/server';
-import Image from 'next/image';
+// import Image from 'next/image';
 
 const Hero = async () => {
   const t = await getI18n();
   return (
     <section className='flex flex-col gap-5 items-center my-10'>
       <h1 className='text-center font-bold text-4xl'>{t('welcomeTitle')}</h1>
-      <Image
+      {/* <Image
         className='rounded-xl'
         src='/hero_photo.jpg'
         alt='hero photo'
         width={700}
         height={300}
-      />
+      /> */}
     </section>
   );
 };
