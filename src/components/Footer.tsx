e10f6efaa6755f8f2bc262ddb9b067dd653495dd
RahymCharyyev <r569837@gmail.com>
2024-04-25 21:46:45 +0500

    ✨feature: Fixed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,28 +6,33 @@ const Footer = async () => {
   const t = await getI18n();
 
   return (
-    <footer className='bg-[#b91f27] py-5 px-10'>
-      <div className='flex flex-wrap justify-between items-center'>
+    <footer className='bg-[#b91f27] py-5'>
+      <div className='flex flex-wrap justify-between items-center md:justify-center md:flex-col'>
         <Link href='/'>
           <Image src='/arion.png' alt='arion logo' width={150} height={35} />
         </Link>
-        <Image
-          className='ml-20'
-          src='/az_icon.svg'
-          alt='azerbaycan flag'
-          width={100}
-          height={25}
-        />
-        <div>
-          <p className='text-white'>{t('reachUs')}</p>
+        <div className='flex flex-col items-center justify-center ml-52 lg:ml-20 md:ml-0'>
+          <Image
+            // className='ml-40 md:ml-0'
+            src='/az_icon.svg'
+            alt='azerbaycan flag'
+            width={100}
+            height={25}
+          />
+          <p className='text-white text-center md:hidden'>
+            {t('allRightsReserved')} © {new Date().getFullYear()}
+          </p>
+        </div>
+        <div className='md:flex md:flex-col md:gap-2'>
+          <p className='text-white md:text-center'>{t('reachUs')}</p>
           <Link
-            className='text-white hover:underline'
+            className='text-white hover:underline md:text-center'
             href={`mailto:ariontrucksaz@inbox.ru`}
           >
             {t('mail')}: ariontrucksaz@inbox.ru
           </Link>
           <Link
-            className='text-white flex gap-2 items-center hover:underline'
+            className='text-white flex gap-2 items-center md:justify-center hover:underline md:text-center'
             href={`tel:+994 55 2982929`}
           >
             {t('phone')}:
@@ -39,11 +44,11 @@ const Footer = async () => {
             />
             +994 55 2982929
           </Link>
+          <p className='text-white text-center hidden md:block'>
+            {t('allRightsReserved')} © {new Date().getFullYear()}
+          </p>
         </div>
       </div>
-      <p className='text-white text-center'>
-        {t('allRightsReserved')} © {new Date().getFullYear()}
-      </p>
     </footer>
   );
 };
